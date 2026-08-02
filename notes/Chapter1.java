7441dbd14d05eba9e0c5e499c409c2af80893f5e
Yuri-S-Study <daigakuikuzo555@gmail.com>
2026-08-03 02:21:48 +0900

    Chapter1_2.java

---
+++ notes/Chapter1.java
@@ -0,0 +1,9 @@
+public class Chapter1 {
+    public static void main(String[] args) {
+        int Ta = 3;
+        int Yo = 5; 
+        int Kotae = Ta * Yo;
+        System.out.println("縦幅3横幅5の長方形の面積は、 " + Kotae);
+    }
+}
+
---
+++ notes/Chapter1_2.java
@@ -0,0 +1,9 @@
+public class Chapter1_2 {
+    public static void main(String[] args) {
+        boolean result = true;
+        char favoriteCharacter = '駆';
+        double pi = 3.14;
+        long number = 314159265853979L;
+        String name = "ミナトの攻撃!敵に15ポイントのダメージを与えた!";
+    }
+}
