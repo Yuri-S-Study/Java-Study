f44741ac5ebdde91ca6c23a64a9e5da8533b5eb1
Yuri-S-Study <daigakuikuzo555@gmail.com>
2026-09-03 04:26:14 +0900

    Chapter6.java

---
+++ notes/Chapter6.java
@@ -0,0 +1,10 @@
+public class Chapter6 {
+  public static void main(String[] args) {
+    // printDataメソッドとfullNameメソッドの呼び出しを書き換えてください
+   Chapter6_2.printData(Chapter6_2.fullName("Kate", "Jones"), 27, 1.6, 50.0);
+   Chapter6_2.printData(Chapter6_2.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
+  }
+  
+  // 以下のメソッドすべてを、Personクラスに移動してください
+ 
+}
