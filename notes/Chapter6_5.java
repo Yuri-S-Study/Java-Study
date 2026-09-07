9cc4415a3a191ce5015c5ce45b179a9926600330
Yuri-S-Study <daigakuikuzo555@gmail.com>
2026-09-08 02:23:53 +0900

    Chapter6_5.java

---
+++ notes/Chapter6_5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Chapter6_5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("何人分の情報を入力しますか：");
+    int n = scanner.nextInt();
+    for (int i = 0; i < n; i++) {
+      System.out.println(i + 1 + "人目");
+      System.out.print("名前：");
+      String firstName = scanner.next();
+      System.out.print("名字：");
+      String lastName = scanner.next();
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+      Chapter6_6.printData(firstName,lastName,age,height,weight);
+    }
+  }
+} 
